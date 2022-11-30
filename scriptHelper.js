14a3b1924a37452ea3ecb2d243f20d36c413c92a
Chris Cleveland <cwc0202@gmail.com>
2022-11-29 21:08:39 -0600

    fetch function complete

--- scriptHelper.js
+++ scriptHelper.js
@@ -16,12 +16,6 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     */
 }
 
-// // validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate. 
-// In scriptHelper.js, you will use validateInput() to complete the formSubmission() function. formSubmission() will take in a 
-// document parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass. Using the values in those strings 
-// and the document parameter for your HTML document, update the shuttle requirements as described below. Make sure to call your 
-// formSubmission() function at the appropriate time in your script.js file!
-
 function validateInput(testInput) {
     if (isNaN(testInput)) return "Not a Number";
     if (testInput == "") return "Empty";
@@ -30,14 +24,6 @@ function validateInput(testInput) {
 }
 
 
-// <div  id="faultyItems" data-testid="faultyItems">
-//     <ol>
-//         <li id="pilotStatus" data-testid="pilotStatus">Pilot Ready</li>
-//         <li id="copilotStatus" data-testid="copilotStatus">Co-pilot Ready</li>
-//         <li id="fuelStatus" data-testid="fuelStatus">Fuel level high enough for launch</li>
-//         <li id="cargoStatus" data-testid="cargoStatus">Cargo mass low enough for launch</li>
-//     </ol>
-// </div>
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     let faultyItems = document.getElementById('faultyItems')
     faultyItems.innerHTML = `
@@ -69,13 +55,15 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then(function (response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+        return response.json()
     });
-
+    console.log(planetsReturned)
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
